3575c28469ee6f302c0a593f285c4c3a7b777be2
jfdenise <jfdenise@dhcp-grenoble-10-166-105-234.fr.oracle.com>
2013-11-04 19:53:55 +0100

    leak tests and tiny long running test module

---
+++ src/test/js/leak/http-leak1.js
@@ -0,0 +1,56 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var perf = require("./common-perf");
+
+var http = require('http');
+
+var body = 'hello world\n';
+var PORT = 9999;
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'content-type': 'text/plain'
+    });
+    res.end(body);
+});
+
+server.listen(PORT, function() {
+    console.log("listen");
+    perf.startPerf(startClient, 100)
+});
+
+function startClient() {
+    perf.actionStart();
+    http.get({
+        port: PORT
+    }, function(res) {
+        res.on('data', function(d) {
+            
+        });
+        res.on('end', function(d) {
+            if (perf.canContinue()) {
+                startClient();
+            }
+        });
+    });
+}
+
---
+++ src/test/js/perf/common-perf.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+var start = 0;
+var count = 0;
+var go = true;
+function startPerf(fstart, time) {
+    start = process.hrtime();
+    setTimeout(function() {
+        go = false;
+    }, time * 1000);
+    fstart();
+    print("Perf test started.");
+}
+exports.startPerf = startPerf;
+function round(n) {
+    return Math.floor(n * 100) / 100;
+}
+
+function dumpResults() {
+    var end = process.hrtime();
+    process.stdout.write('\n');
+    console.log('num of actions %d ', count);
+    var elapsed = [end[0] - start[0], end[1] - start[1]];
+    var ns = elapsed[0] * 1E9 + elapsed[1];
+    var nsper = round(ns / count);
+    console.log('%d ns per action (lower is better)', nsper);
+    var readsper = round(count / (ns / 1E9));
+    console.log('%d actions per sec (higher is better)', readsper);
+}
+exports.dumpResults = dumpResults;
+function canContinue() {
+    if (!go) {
+        dumpResults();
+        process.exit(0);
+    }
+    return true;
+}
+exports.canContinue = canContinue;
+function actionStart() {
+    count++;
+    if (!(count % 1000)) {
+        process.stdout.write('.');
+    }
+}
+exports.actionStart = actionStart;
